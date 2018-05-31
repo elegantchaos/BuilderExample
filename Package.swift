d4be6aaabf1c866a7144eba480303741e61901f5
Sam Deane <sam@elegantchaos.com>
2018-05-31 22:36:39 +0100

    Updated to released versions of branches.

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
       .package(url: "https://github.com/elegantchaos/BuilderToolExample.git", from: "1.0.6"),
 
       // support library we're going to use in the configuration target
-      .package(url: "https://github.com/elegantchaos/BuilderConfiguration.git", .branch("issue/1-value-based-settings")),
+      .package(url: "https://github.com/elegantchaos/BuilderConfiguration.git", from: "1.1.3"),
 
       // builder itself
-      .package(url: "https://github.com/elegantchaos/Builder.git", .branch("issue/12-value-based-settings")),
+      .package(url: "https://github.com/elegantchaos/Builder.git", from: "1.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
