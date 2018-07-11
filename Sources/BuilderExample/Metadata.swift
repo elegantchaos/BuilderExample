a110b9acfbd1bee55a6cae5a99ee626fa6d5fcad
Sam Deane <sam@elegantchaos.com>
2018-07-11 14:36:04 +0100

    use Builder 1.0.9

--- Sources/BuilderExample/Metadata.swift
+++ Sources/BuilderExample/Metadata.swift
@@ -5,4 +5,4 @@ struct Metadata {
     let commit: String
 }
 
-let BuilderExampleMetadata = Metadata(version: "1.0", build: "23", tags: "heads/master", commit: "10dc8a51ca9b9897b664bf36c8b69d5d41699e3b")
+let BuilderExampleMetadata = Metadata(version: "1.0.0-4-g43c539c", build: "25", tags: "heads/master", commit: "43c539cc91d7891d4d5ae9f5c86dc06ef816fd85")
